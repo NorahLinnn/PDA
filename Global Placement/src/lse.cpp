72a8961b188f1d838f4f9702407b6686f55a84ba
NorahLinnn <84172062+NorahLinnn@users.noreply.github.com>
2024-02-20 00:57:55 +0800

    Update lse.cpp

--- Global Placement/src/lse.cpp
+++ Global Placement/src/lse.cpp
@@ -13,24 +13,16 @@ LSE::LSE(wrapper::Placement &placement,double &f_value, vector<double> &g_value,
     double bin_width = coreWidth/bin_num_width;
     double bin_height = coreHeight/bin_num_height;
     vector<double> c_vector(_placement.numModules(), 0);
-    // for(size_t i = 0; i<_placement.numModules(); ++i)
-    // {
-    //     auto m = _placement.module(i);
-    //     target_density += m.area();
-    //     c_vector[i] = m.area()/(bin_width*bin_height);
-    // }
-    // target_density = target_density/(coreWidth*coreHeight);
+    
 }
 void LSE::create_exp_vector(const vector<double> &x, vector<double> &exp_vector, double eta)
 {
-    // std::cout << exp_vector.size() << std::endl;
-    // std::cout << x.size() << std::endl;
+    
     for (size_t i = 0; i < _placement.numModules(); ++i)
     {
         if (_placement.module(i).isFixed())
         {
             auto m = _placement.module(i);
-            // std::cout << "i: " << i*4 << " " << i*4+1 << " " << i*4+2 << " " << i*4+3 << std::endl;
             exp_vector[i*4] = exp(m.centerX()/eta);
             exp_vector[i*4+1] = exp(-m.centerX()/eta);
             exp_vector[i*4+2] = exp(m.centerY()/eta);
@@ -38,17 +30,13 @@ void LSE::create_exp_vector(const vector<double> &x, vector<double> &exp_vector,
         }
         else
         {
-            // std::cout << "i: " << i*4 << " " << i*4+1 << " " << i*4+2 << " " << i*4+3 << std::endl;
-            // std::cout << "i: " << i*2 << " " << i*2+1 << std::endl;
-            // if(i*4+3>exp_vector.size()) std::cout<<"i*4: " <<i << std::endl;
-            // if(i*2+1>x.size()) std::cout<<"i*2: " <<i << std::endl;
+            
             auto m = _placement.module(i);
             exp_vector[i*4] = exp(((x[i*2])+m.width()/2)/eta);
             exp_vector[i*4+1] = exp(-((x[i*2])+m.width()/2)/eta);
             exp_vector[i*4+2] = exp(((x[i*2+1])+m.height()/2)/eta);
             exp_vector[i*4+3] = exp(-((x[i*2+1])+m.height()/2)/eta);  
         }
-        // std::cout << "i: " << exp_vector[i*4] << " " << exp_vector[i*4+1] << " " << exp_vector[i*4+2] << " " << exp_vector[i*4+3] << std::endl;
 
     }
     // std::cout << "finish" << std::endl;
@@ -60,7 +48,6 @@ double LSE::WL(const vector<double> &x, double eta, vector<double> &exp_vector, 
     double return_value=0;
     for(size_t i=0; i< _placement.numNets(); ++i)
     {
-        // std::cout << "i: " << std::endl;
         auto current_net = _placement.net(i);
         for(size_t j = 0; j<current_net.numPins();++j)
         {
@@ -71,26 +58,19 @@ double LSE::WL(const vector<double> &x, double eta, vector<double> &exp_vector, 
             exp_sum[i][3] += exp_vector[current_pin.moduleId()*4+3];
         }
         return_value += (log(exp_sum[i][0])+log(exp_sum[i][1])+log(exp_sum[i][2])+log(exp_sum[i][3]));   
-        // std::cout << return_value << std::endl;   
     }
     return return_value*eta;
 }
-// vector<double> LSE::WL_cal(const vector<double> &x, double eta, double &wl, vector<double> &exp_vector,vector<double> &cal_vector, vector<vector<double>> &exp_sum)
 void LSE::WL_cal(const vector<double> &x, double eta, double &wl, vector<double> &exp_vector,vector<double> &cal_vector, vector<double> &wl_cal_result, vector<vector<double>> &exp_sum)
 {
-    // vector<double> wl_cal_result(_placement.numModules()*2,0);
-    // create_cal_vector(x, exp_vector, cal_vector,eta, exp_sum);
+    
     for(size_t i=0; i< _placement.numNets(); ++i)
     {
-        // std::cout << "i: " << std::endl;
         auto current_net = _placement.net(i);
         for(size_t j = 0; j<current_net.numPins();++j)
         {
             auto current_pin = current_net.pin(j);
-            // wl_cal_result[current_pin.moduleId()*2] += (1/lseloss_1)*(1/eta)*exp_vector[current_pin.moduleId()*4];
-            // wl_cal_result[current_pin.moduleId()*2] -= (1/lseloss_2)*(1/eta)*exp_vector[current_pin.moduleId()*4+1];
-            // wl_cal_result[current_pin.moduleId()*2+1] += (1/lseloss_3)*(1/eta)*exp_vector[current_pin.moduleId()*4+2];
-            // wl_cal_result[current_pin.moduleId()*2+1] -= (1/lseloss_4)*(1/eta)*exp_vector[current_pin.moduleId()*4+3];
+            
             wl_cal_result[current_pin.moduleId()*2] += exp_vector[current_pin.moduleId()*4]/exp_sum[i][0];
             wl_cal_result[current_pin.moduleId()*2] -= exp_vector[current_pin.moduleId()*4+1]/exp_sum[i][1];
             wl_cal_result[current_pin.moduleId()*2+1] += exp_vector[current_pin.moduleId()*4+2]/exp_sum[i][2];
@@ -141,10 +121,8 @@ double LSE::density_constraint(const vector<double> &x, double beta, vector<doub
 {
     double return_value = 0;
     double return_cal_value = 0;
-    // vector<double> theta_vector(_placement.numModules()*2, 0);
     vector<vector<vector<double>>> theta_vector(_placement.numModules()*2, vector<vector<double>>(bin_num_width,vector<double>(bin_num_height,0)));
     vector<vector<vector<double>>> theta_cal_vector(_placement.numModules()*2, vector<vector<double>>(bin_num_width,vector<double>(bin_num_height,0)));
-    // vector<double> theta_cal_vector(_placement.numModules()*2,0);
     vector<double> c_vector(_placement.numModules(), 0);
     
     double coreWidth = _placement.boundryRight() - _placement.boundryLeft();
@@ -156,7 +134,6 @@ double LSE::density_constraint(const vector<double> &x, double beta, vector<doub
     double target_density = 0;
     vector<double> theta_sum(_placement.numModules(), 0);
     vector<double> density_result(bin_num_height*bin_num_width, 0);
-    // vector<double> density_cal_result(_placement.numModules()*2, 0);
     for (size_t i = 0; i < _placement.numModules(); ++i)
     {
         auto current_module = _placement.module(i);
@@ -193,130 +170,67 @@ double LSE::density_constraint(const vector<double> &x, double beta, vector<doub
                 if(d_X >= 0 && d_X <= (bin_width/2+w_i/2))
                 {
                     theta_vector[i*2][a][b] = condtionA(alphaX, d_X);
-                    // theta_vector[i*2][a][b] = 1-alphaX*pow(d_X,2);
-                    
-                    // theta_cal_vector[i*2][a][b] = condtionA_cal(alphaX,x[i*2]+w_i/2,(boundryLeft+a*bin_width)+bin_width/2);
                 }else if(d_X >= (bin_width/2+w_i/2) && d_X <= (bin_width+w_i/2))
                 {
                     theta_vector[i*2][a][b] = condtionB(betaX,d_X,bin_width,w_i);
-                    // theta_vector[i*2][a][b] = betaX*pow((d_X-bin_width-w_i/2),2);
-                    
-                    // theta_cal_vector[i*2][a][b] = condtionB_cal(betaX,d_X, bin_width,w_i,x[i*2]+w_i/2,(boundryLeft+a*bin_width)+bin_width/2);
                 }else if((bin_width+w_i/2) <= d_X)
                 {
-                    // theta_vector[i*2][a][b] = condtionC();
                     theta_vector[i*2][a][b] = 0;
-                    // theta_cal_vector[i*2][a][b] = condtionC_cal();
                 }
 
                 if(d_Y >= 0 && d_Y <= (bin_height/2+h_i/2))
                 {
                     theta_vector[i*2+1][a][b] = condtionA(alphaY, d_Y);
-                    // theta_vector[i*2+1][a][b] = 1-alphaY*pow(d_Y,2);
                     
-                    // theta_cal_vector[i*2+1][a][b] = condtionA_cal(alphaY, x[i*2+1]+h_i/2,(boundryBottom+b*bin_height)+bin_height/2);
                 }else if(d_Y >= (bin_height/2+h_i/2) && d_Y <= (bin_height+h_i/2))
                 {
                     theta_vector[i*2+1][a][b] = condtionB(betaY, d_Y, bin_height, h_i);
-                    // theta_vector[i*2+1][a][b] = betaY*pow((d_Y-bin_height-h_i/2),2);
-                    
-                    // theta_cal_vector[i*2+1][a][b] = condtionB_cal(betaY, d_Y, bin_height, h_i,x[i*2+1]+h_i/2,(boundryBottom+b*bin_height)+bin_height/2);
                 }else if((bin_height+h_i/2) <= d_Y)
                 {
-                    // theta_vector[i*2+1][a][b] = condtionC();
                     theta_vector[i*2+1][a][b] = 0;
-                    // theta_cal_vector[i*2+1][a][b] = condtionC_cal();
                 }
                 //grad
                 if(d_X >= 0 && d_X <= (bin_width/2+w_i/2))
                 {
-                    // theta_vector[i*2][a][b] = condtionA(alphaX, d_X);
-                    // theta_cal_vector[i*2][a][b] = condtionA_cal(alphaX,x[i*2]+w_i/2,(boundryLeft+a*bin_width)+bin_width/2)*theta_vector[i*2+1][a][b];
                     
-                    // theta_cal_vector[i*2][a][b] = condtionA_cal(alphaX,x[i*2]+w_i/2,(boundryLeft+a*bin_width)+bin_width/2)*theta_vector[i*2+1][a][b];
-                    // if((x[i*2]+w_i/2)-(boundryLeft+a*bin_width+bin_width/2) >= 0 )
-                    // {
-                    //     theta_cal_vector[i*2][a][b] = -2*alphaX*(d_X)*theta_vector[i*2+1][a][b];
-                    // }else{
-                    //     theta_cal_vector[i*2][a][b] = -2*alphaX*(d_X)*(-1)*theta_vector[i*2+1][a][b];
-                    // }
                     theta_cal_vector[i*2][a][b] = condtionA_cal(alphaX,x[i*2]+w_i/2,(boundryLeft+a*bin_width)+bin_width/2)*theta_vector[i*2+1][a][b];
 
-                    // -2*alphaX*(d_X)*(d_X/(x_i-x_b));
+                    
                 }else if(d_X >= (bin_width/2+w_i/2) && d_X <= (bin_width+w_i/2))
                 {
-                    // theta_vector[i*2][a][b] = condtionB(betaX,d_X,bin_width,w_i);
-                    // theta_cal_vector[i*2][a][b] = condtionB_cal(betaX,d_X, bin_width,w_i,x[i*2]+w_i/2,(boundryLeft+a*bin_width)+bin_width/2)*theta_vector[i*2+1][a][b];
-                    // theta_cal_vector[i*2][a][b] = condtionB_cal(betaX,d_X, bin_width,w_i,x[i*2]+w_i/2,(boundryLeft+a*bin_width)+bin_width/2)*theta_vector[i*2+1][a][b];
+                    
                     theta_cal_vector[i*2][a][b] = condtionB_cal(betaX,d_X, bin_width,w_i,x[i*2]+w_i/2,(boundryLeft+a*bin_width)+bin_width/2)*theta_vector[i*2+1][a][b];
-                    // betaX*(2*d_X-2*bin_width-w_i)*(abs(x_i-x_b)/(x_i-x_b));
-                    // if((x[i*2]+w_i/2)-(boundryLeft+a*bin_width+bin_width/2) >= 0 )
-                    // {
-                    //     theta_cal_vector[i*2][a][b] = betaX*(2*d_X-2*bin_width-w_i)*theta_vector[i*2+1][a][b];
-                    // }else{
-                    //     theta_cal_vector[i*2][a][b] = betaX*(2*d_X-2*bin_width-w_i)*(-1)*theta_vector[i*2+1][a][b];
-                    // }
+                    
                 }else if((bin_width+w_i/2) <= d_X)
                 {
-                    // theta_vector[i*2][a][b] = condtionC();
-                    // theta_cal_vector[i*2][a][b] = condtionC_cal();
+                    
                     theta_cal_vector[i*2][a][b] = 0;
                 }
 
                 if(d_Y >= 0 && d_Y <= (bin_height/2+h_i/2))
                 {
-                    // theta_vector[i*2+1][a][b] = condtionA(alphaY, d_Y);
-                    // theta_cal_vector[i*2+1][a][b] = condtionA_cal(alphaY, x[i*2+1]+h_i/2,(boundryBottom+b*bin_height)+bin_height/2)*theta_vector[i*2][a][b];
+                    
                     theta_cal_vector[i*2+1][a][b] = condtionA_cal(alphaY, x[i*2+1]+h_i/2,(boundryBottom+b*bin_height)+bin_height/2)*theta_vector[i*2][a][b];
-                    // if((x[i*2+1]+h_i/2)-(boundryBottom+b*bin_height+bin_height/2)>=0)
-                    // {
-                    //     theta_cal_vector[i*2+1][a][b] = -2*alphaY*(d_Y)*theta_vector[i*2][a][b];
-                    // }else{
-                    //     theta_cal_vector[i*2+1][a][b] = -2*alphaY*(d_Y)*(-1)*theta_vector[i*2][a][b];
-                    // }
+                    
 
                 }else if(d_Y >= (bin_height/2+h_i/2) && d_Y <= (bin_height+h_i/2))
                 {
-                    // theta_vector[i*2+1][a][b] = condtionB(betaY, d_Y, bin_height, h_i);
-                    // theta_cal_vector[i*2+1][a][b] = condtionB_cal(betaY, d_Y, bin_height, h_i,x[i*2+1]+h_i/2,(boundryBottom+b*bin_height)+bin_height/2)*theta_vector[i*2][a][b];
+                    
                     theta_cal_vector[i*2+1][a][b] = condtionB_cal(betaY, d_Y, bin_height, h_i,x[i*2+1]+h_i/2,(boundryBottom+b*bin_height)+bin_height/2)*theta_vector[i*2][a][b];
-                    // if((x[i*2+1]+h_i/2)-(boundryBottom+b*bin_height+bin_height/2) >= 0)
-                    // {
-                    //     theta_cal_vector[i*2+1][a][b] = beta*(2*(d_Y)-2*bin_height-h_i)*theta_vector[i*2][a][b];
-                    // }else{
-                    //     theta_cal_vector[i*2+1][a][b] = beta*(2*(d_Y)-2*bin_height-h_i)*(-1)*theta_vector[i*2][a][b];
-                    // }
+                    
                     
                 }else if((bin_height+h_i/2) <= d_Y)
                 {
-                    // theta_vector[i*2+1][a][b] = condtionC();
-                    // theta_cal_vector[i*2+1][a][b] = condtionC_cal();
                     theta_cal_vector[i*2+1][a][b] = 0;
                 }
-                // std::cout << "x: " << theta_vector[i*2][a][b] << "y: " << theta_vector[i*2+1][a][b] << std::endl;
-                // std::cout << "y: " << theta_vector[i*2+1][a][b] << std::endl;
-                // theta_sum[i] += (theta_vector[i*2][a][b]*theta_vector[i*2+1][a][b]);
                 c_vector[i] = current_module.area()/(bin_width*bin_height);
-                // target_density += m.area();
                 
                 density_result[bin_num_width*a+b] += c_vector[i]*theta_vector[i*2][a][b]*theta_vector[i*2+1][a][b];
             }
-            // target_density = target_density/(coreWidth*coreHeight);
             return_value += pow(density_result[bin_num_width*a+b]-target_density,2);
-            // for (size_t i = 0; i < _placement.numModules(); ++i)
-            // {
-            //     if(_placement.module(i).isFixed())
-            //         continue;
-            //     auto m = _placement.module(i);
-                
-            //     density_cal_result[i*2] += 2*c_vector[i]*theta_cal_vector[i*2][a][b]*(density_result[bin_num_width*a+b]-target_density)*beta;
-            //     density_cal_result[i*2+1] += 2*c_vector[i]*theta_cal_vector[i*2+1][a][b]*(density_result[bin_num_width*a+b]-target_density)*beta;
-            // }
+            
         }
-        // c_vector[i] = m.area()/(bin_width*bin_height);
-        // c_vector[i] = 1;
-        // std::cout << "theta sum: " << theta_sum << std::endl;
-        // std::cout << c_vector[i] << std::endl;
+        
         
     }
     for (size_t a = 0; a < bin_num_width; ++a)
@@ -327,19 +241,14 @@ double LSE::density_constraint(const vector<double> &x, double beta, vector<doub
             {
                 if(_placement.module(i).isFixed())
                     continue;
-                // auto current_module = _placement.module(i);
                 
                 density_cal_result[i*2] += 2*c_vector[i]*theta_cal_vector[i*2][a][b]*(density_result[bin_num_width*a+b]-target_density)*beta;
                 density_cal_result[i*2+1] += 2*c_vector[i]*theta_cal_vector[i*2+1][a][b]*(density_result[bin_num_width*a+b]-target_density)*beta;
             }
-            // return_value += pow(density_result[bin_num_width*a+b]-target_density,2);
+            
         }
     }
-    // for(auto v:density_cal_result)
-    // {
-    //     std::cout << v << std::endl;
-    // }
-    // return std::make_pair(return_value*beta, density_cal_result);
+    
     return return_value*beta;
     
 }
@@ -359,46 +268,25 @@ void LSE::lseloss(const vector<double> &x, double eta, double beta)
     {
         dc = density_constraint(x,beta, density_cal_result);
         int idx = 0;
-        // f_value = dc.first+wl;
-        // std::cout<<dc/beta << std::endl;
+        
         f_value = dc+wl;
         for(auto& v: g_value)
         {
-            // g_value[idx] = wl_cal[idx]+dc.second[idx];
             g_value[idx] = wl_cal_result[idx]+density_cal_result[idx];
-            
-            // std::cout << "wl_cal: " << wl_cal[idx] << " ,dc_cal: " << dc.second[idx] << std::endl;
             idx += 1;
         }
-        // f_value = dc;
-        // g_value = density_cal_result;
     }else{
         f_value = wl;
         g_value = wl_cal_result;
     }
     
-    // std::cout << wl <<  std::endl;
-    // std::cout << "dc: " << dc.first << std::endl;
-    // f_value = wl;
-    // g_value = wl_cal;
     
-    // dc = density_constraint(x,beta);
-    
-    // f_value = dc.first;
-    // g_value = dc.second;
-    // g_value = dc.second+wl_cal;
-    // g_value.reserve(wl_cal.size());
-
-    // std::transform(wl_cal.begin(), wl_cal.end(), dc.second.begin(), std::back_inserter(g_value), std::plus<double>());
 
 }
 void LSE::evaluateFG(const vector<double> &x, double &f, vector<double> &g)
 {
-    // f = 3 * x[0] * x[0] + 2 * x[0] * x[1] + 2 * x[1] * x[1] + 7; // objective function
-    // g[0] = 6 * x[0] + 2 * x[1];                                  // gradient function of X
-    // g[1] = 2 * x[0] + 4 * x[1];                                  // gradient function of Y
+   
     lseloss(x, eta,beta);
-    // std::cout << f_value <<  " " <<  g_value[0] <<  std::endl;
     f = f_value;
     g = g_value;
 }
